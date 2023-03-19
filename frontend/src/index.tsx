4b782aae9ad3672b844d04f9f3fe6030519f6e39
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-19 22:19:18 +0900

    frontend: first commit - react+vite

---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export function Index() {
+  return (
+    <div className="h-screen bg-slate-900 text-slate-300 p-4 text-xl">
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
