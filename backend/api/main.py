2863e8836d0966963f24dd356fc317ec69de40fb
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-20 13:26:19 +0900

    backend: api/main.py - add CORS

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
-
 from api.routers.auths import router as auth_router
 from api.routers.tasks import router as task_router
 from api.routers.users import router as user_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(task_router)
