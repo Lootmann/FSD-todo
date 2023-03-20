27736bcb1233da4609a39c4dfa152be5ba827cec
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-20 15:23:16 +0900

    frontend: auth/index - when you are logged in, redirect '/' instead of login,signup form

--- frontend/src/auths/index.tsx
+++ frontend/src/auths/index.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Header } from "../components/header";
+import { isLogin } from "../apis/auth";
 import { Outlet } from "react-router-dom";
 
 export function Index() {
+  // TODO: it seems wrong :D
+  useEffect(() => {
+    if (isLogin()) window.location.href = "/";
+  }, []);
+
   return (
     <div className="h-screen flex flex-col bg-zinc-900 text-slate-200 text-xl">
       <Header />
