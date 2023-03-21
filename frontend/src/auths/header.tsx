c92d01c0c70b8690281a533e9e8e733afea4815c
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-21 23:41:10 +0900

    frontend: fix header css

--- frontend/src/auths/header.tsx
+++ frontend/src/auths/header.tsx
@@ -5,9 +5,7 @@ export function AuthHeader() {
   return (
     <div className="flex gap-4 items-baseline bg-zinc-800 shadow-md px-6 py-1">
       <h1 className="text-2xl mr-4">
-        <span className="text-2xl hover:bg-zinc-600 px-1 rounded-md transition-all">
-          Todo Master
-        </span>
+        <span className="text-2xl px-1 rounded-md">Todo Master</span>
       </h1>
 
       <Link
