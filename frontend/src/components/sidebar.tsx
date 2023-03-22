499f0ee7a4f380e1077535a78c60ad3207904310
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-23 00:58:29 +0900

    frontend: sidebar.tsx - add filtered title

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function Sidebar() {
   return (
     <div className="h-full w-52 flex flex-col bg-zinc-850 px-4 pt-4">
       <ul>
+        {/* TODO: show all tasks */}
+        <li className="hover:bg-zinc-700 px-1 my-1 rounded-md">
+          <Link to={`/tasks`}>Inbox</Link>
+        </li>
+
+        {/* TODO: show all today's tasks */}
         <li className="hover:bg-zinc-700 px-1 my-1 rounded-md">Today</li>
+
+        {/* TODO: show all future's tasks */}
         <li className="hover:bg-zinc-700 px-1 my-1 rounded-md">Upcoming</li>
-        <li className="hover:bg-zinc-700 px-1 my-1 rounded-md">hoge</li>
-        <li className="hover:bg-zinc-700 px-1 my-1 rounded-md">hoge</li>
       </ul>
 
       <div className="my-6">
