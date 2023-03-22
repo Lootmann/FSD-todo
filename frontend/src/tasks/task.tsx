d1b1785b488139c85cd995b8abc494c39703e044
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-23 03:03:54 +0900

    frontend: tasks/task.tsx - add react-hook-form

--- frontend/src/tasks/task.tsx
+++ frontend/src/tasks/task.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { API_BACKEND_URL } from "../settings";
 import { getAuthToken } from "../apis/auth";
 import { getPriorityMark } from "../utils/svg";
+import { useForm } from "react-hook-form";
 
 export function Task({ task, handleRefresh }: TaskProp) {
   function onSubmit(
@@ -35,9 +36,25 @@ export function Task({ task, handleRefresh }: TaskProp) {
       });
   }
 
+  // NOTE: react-hook-form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  console.log(watch("title"), watch("description"));
+
   // TODO: function delete and update task
-  function deleteTask() {}
-  function updateTask() {}
+  function deleteTask(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    console.log(">>> delete task");
+  }
+
+  function updateTask() {
+    console.log(">>> update task");
+  }
 
   // task detail Modal
   const [openModal, setOpenModal] = useState<boolean>(false);
@@ -60,34 +77,52 @@ export function Task({ task, handleRefresh }: TaskProp) {
         {task.title}
       </li>
 
-      {/* TODO: react-hook-form */}
       {openModal && (
         <div
           className="absolute inset-0 w-full h-full py-20 flex items-center justify-center bg-opacity-80 bg-zinc-700"
           onClick={() => handleModal(false)}
         >
-          <div
+          <form
+            onSubmit={handleSubmit(updateTask)}
             className="relative w-2/5 h-full flex flex-col bg-zinc-900 opacity-100 p-4 text-xl rounded-md"
             onClick={(e) => e.stopPropagation()}
           >
-            <header>{task.id}.Modal</header>
+            <header className="text-xl mb-4">Update Task</header>
 
-            <div className="flex-1 flex flex-col">
-              <p>{task.title}</p>
-              <p>{task.description}</p>
+            <div className="flex-1 flex flex-col gap-4 ">
+              <input
+                id="title"
+                defaultValue={task.title}
+                {...register("title")}
+                className="bg-zinc-900 hover:border-zinc-700 text-2xl px-2 rounded-md"
+              />
+              <input
+                defaultValue={task.description}
+                {...register("description")}
+                className="bg-zinc-900 hover:border-zinc-700 px-2 rounded-md"
+              />
+              {/* TODO: expired_t */}
             </div>
 
-            {/* TODO: update task, then refresh */}
-            {/* TODO: delete task, then refresh */}
             <footer className="flex justify-around">
-              <p className="bg-red-700 text-black text-2xl px-2 rounded-md">
+              {/* TODO: delete task, then refresh */}
+              <button
+                type="submit"
+                onClick={(e) => deleteTask(e)}
+                className="bg-red-700 text-black text-2xl px-2 rounded-md"
+              >
                 Delete
-              </p>
-              <p className="bg-yellow-700 text-black text-2xl px-2 rounded-md">
-                Update
-              </p>
+              </button>
+
+              {/* TODO: update task, then refresh */}
+              <input
+                type="submit"
+                className="bg-yellow-700 text-black text-2xl px-2 rounded-md"
+                onSubmit={handleSubmit(updateTask)}
+                value="Update"
+              />
             </footer>
-          </div>
+          </form>
         </div>
       )}
     </>
