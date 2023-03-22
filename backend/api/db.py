b26a0c05b282a718c61511a22f00344c5074e841
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-22 13:26:49 +0900

    backend: migrate.py - add create test user, what is the correct way?

--- backend/api/db.py
+++ backend/api/db.py
@@ -14,8 +14,16 @@ def get_db():
 
 
 if __name__ == "__main__":
+    from api.cruds.auths import get_hashed_password
+
     User.metadata.drop_all(engine)
     User.metadata.create_all(engine)
 
     Task.metadata.drop_all(engine)
     Task.metadata.create_all(engine)
+
+    # NOTE: create test user
+    with Session(engine) as session:
+        user = User(username="toortoor", password=get_hashed_password("toortoor"))
+        session.add(user)
+        session.commit()
